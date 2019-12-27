c0d813cab3aa9084cc0f2d9ecb1dfedc9a273dae
manhhung241199 <hungvmgch17607@fpt.edu.vn>
2019-12-27 21:11:41 +0700

    first commit

---
+++ delete.php
@@ -0,0 +1,11 @@
+<?php
+require("connect.php");
+$id = $_POST['productid'];
+$sql = "DELETE FROM product WHERE productid = '$id'";
+pg_query($conn,$sql); 
+?>
+
+<script>
+    alert("Delete successfully!!");
+    window.location.href = "/managing.php";
+</script>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/style.css">
+    <title>Login</title>
+</head>
+
+<body>
+    <div class="content">
+        <h1>Login</h1>
+        <form action="managing.php" method="POST">
+            <input class="input-information" type="text" name="aduser" placeholder="Username"> <br>
+            <input class="input-information" type="password" name="adpass" placeholder="Password"> <br>
+            <input class="input-information" type="submit" value="Login">
+        </form>
+</body>
+
+</html>
