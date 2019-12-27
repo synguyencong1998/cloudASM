024d6cbbce78f12b7cee8f62ee9fcafb1652d81c
manhhung241199 <hungvmgch17607@fpt.edu.vn>
2019-12-27 22:29:43 +0700

    second commit

--- managing.php
+++ managing.php
@@ -57,7 +57,7 @@ if(isset($_POST["aduser"]) && isset($_POST["adpass"]))
                     <td class="info"><?php echo $row['proname']?></td> 
                     <td class="info"><?php echo $row['price']?></td> 
                     <td class="info"><?php echo $row['descrip']?></td> 
-                    <td class="info"><?php echo $row['address']?></td> 
+                    <td class="info"><?php echo $row['img']?></td> 
                     <td class="info">
                         <form action='/delete.php' method="POST" onsubmit="return confirmDelete();">
                             <input type='hidden' name='productid' value='<?php echo $row['productid']?>'>
