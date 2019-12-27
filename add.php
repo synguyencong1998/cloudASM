e24e0fe5af4a73ab8d2354059cb593335834c065
manhhung241199 <hungvmgch17607@fpt.edu.vn>
2019-12-27 23:22:26 +0700

    second commit

--- add.php
+++ add.php
@@ -49,15 +49,12 @@
                             pg_query($conn,$sql);
 
                             $uploads_dir = '/images';
-                            foreach ($_FILES["images"]["error"] as $key => $error) {
-                                if ($error == UPLOAD_ERR_OK) {
-                                    $tmp_name = $_FILES["images"]["tmp_name"][$key];
-                                    // basename() may prevent filesystem traversal attacks;
-                                    // further validation/sanitation of the filename may be appropriate
-                                    $name = basename($_FILES["images"]["name"][$key]);
-                                    move_uploaded_file($tmp_name, "$uploads_dir/$name");
-                                }
-                            }
+
+                            $tmp_name = $_FILES["images"]["tmp_name"];
+                            // basename() may prevent filesystem traversal attacks;
+                            // further validation/sanitation of the filename may be appropriate
+                            $name = basename($_FILES["images"]["name"]);
+                            move_uploaded_file($tmp_name, "$uploads_dir/$name");
 
                             ?> 
                                 <script>
