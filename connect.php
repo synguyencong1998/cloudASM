5553043bfe910a6e97095c10953aca28085d05aa
nguyencongsy <syncgch17424@fpt.edu.vn>
2019-12-28 14:23:21 +0700

    connect commit

--- connect.php
+++ connect.php
@@ -1,5 +1,3 @@
 <?php
-$conn = pg_connect("host=ec2-107-20-239-47.compute-1.amazonaws.com
- user=awouysqgoiswyd password=e9e33a445d5a1b7e08c203938199144d4e3d19e2cb53ff90f6551c618ffafa38") 
-or die("Connect Unsuccessful");
+$conn = pg_connect("ec2-107-20-239-47.compute-1.amazonaws.com port=5432dbname=d658q04afrj8vhuser=awouysqgoiswydpassword=e9e33a445d5a1b7e08c203938199144d4e3d19e2cb53ff90f6551c618ffafa38") or die("Connect Unsuccessful");
 ?>
