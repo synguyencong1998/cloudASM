d3bc11dc3340fe274d3a32775e18282497deeb58
nguyencongsy <syncgch17424@fpt.edu.vn>
2019-12-28 14:11:14 +0700

    connect commit

--- add.php
+++ add.php
@@ -6,24 +6,119 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/style.css">
-    <title>Add</title>
+    <title>Adding into database</title>
+    <script src="https://kit.fontawesome.com/3fa7701fe0.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 
 <body>
-    <div class="content">
-        <h1>Adding Product Form</h1>
-        <?php 
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#"><i class="fas fa-user-plus"></i></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+              
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                  </ul>
+                  <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                  </form>
+                </div>
+              </nav>
+
+    <div class="body">
+
+        <form action="add.php" method="POST">
+            <div class="container">
+                    <form class="form-horizontal" action="/admin/add/student" method="POST">
+            
+                        <div class="register-new-student">
+                            <div class="row">
+                                <div class="col-md-3"></div>
+                                <div class="col-md-6">
+                                    <h2>Create New Products</h2>
+                                    <hr>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- First Name -->
+                        <div class="row">
+                            <div class="col-md-3 field-label-responsive">
+                                <label for="name">Name</label>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+                                        <div class="input-group-addon" style="width: 2.6rem"><i class="far fa-user"></i></div>
+                                        <input type="text" name="proname" class="form-control" id="name" placeholder="Name" required autofocus>
+                                    </div>
+                                </div>
+                            </div>                       
+                        </div>     
+                        
+                         <!-- First Name -->
+                         <div class="row">
+                                <div class="col-md-3 field-label-responsive">
+                                    <label for="name">Price</label>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+                                            <div class="input-group-addon" style="width: 2.6rem"><i class="fas fa-money-check-alt"></i></div>
+                                            <input type="text" name="price" class="form-control" id="name" placeholder="Price" required autofocus>
+                                        </div>
+                                    </div>
+                                </div>                       
+                            </div>      
+
+                             <!-- First Name -->
+                        <div class="row">
+                                <div class="col-md-3 field-label-responsive">
+                                    <label for="name">Description</label>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+                                            <div class="input-group-addon" style="width: 2.6rem"><i class="fas fa-user-edit"></i></div>
+                                            <input type="text" name="descrip" class="form-control" id="name" placeholder="Description" required autofocus>
+                                        </div>
+                                    </div>
+                                </div>                       
+                            </div>     
+                            
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <button  class="btn btn-success"><a href="/managing.php" style="text-decoration: none; color: white;">Back</a></button>
+                                </div>
+                                <div class="col-md-6">
+                                    <button type="submit"   value="Add" class="btn btn-success"><i class="fa fa-user-plus"></i> Create</button>
+                                </div>
+                            </div>
+                        
+                    </form>
+            </div>
+        </form>
+
+
+
+
+    <?php 
         require("connect.php");   
         if(isset($_POST["submit"]))
             {
                 $name = $_POST["proname"];
                 $price = $_POST["price"];
-                $descript = $_POST["descript"];
-                if ($name == ""||$price == ""|| $descript == "") 
+                $descrip = $_POST["descrip"];
+                if ($name == ""||$price == ""|| $descrip == "") 
                     {
                         ?>
                         <script>
-                            alert("Product information should not be blank!!");
+                            alert("Please input product information!!");
                         </script>
                         <?php
                     }
@@ -41,36 +136,31 @@
                         }
                         else
                         {
-                            // image file directory
-                            if(isset($_FILES['images'])) {
-                                $img = './images/' . $_FILES['images']['name'];
-                                move_uploaded_file($_FILES['images']['tmp_name'], './images/' . $_FILES['images']['name']);
-                            }
-                            
-                            $sql = "INSERT INTO product(proname, price, descript, img) VALUES ('$name','$price','$descript', '$img')";
+                            $sql = "INSERT INTO product(proname, price, descrip) VALUES ('$name','$price','$descrip')";
                             pg_query($conn,$sql);
-
                             ?> 
                                 <script>
-                                    alert("Added successful!");
+                                    alert("Added into Database successful!!");
                                     window.location.href = "/managing.php";
                                 </script>
                             <?php
                         }
                     }
             }
-			?>
-        <form action="add.php" method="POST" enctype="multipart/form-data">
-            <input class="input-information" type="text" width="300" height="100" name="proname" placeholder="Name"> <br>
-            <input class="input-information" type="text" width="300" height="100" name="price" placeholder="Price"> <br>
-            <input class="input-information" type="text" width="300" height="100" name="descript" placeholder="Description"> <br>
+            ?>
+            
 
-            <div>Select images: <input type="file" name="images"></div><br>
-            <button type="submit" value="Add" name="submit">Add</button>
-        </form>
-        
-        <button><a href="/managing.php">Back</a></button>
+           
     </div>
+          
+          
+
+      
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
